8459a355684646aea8d0b32cb54e20bfe19219dc
Dezi <dezi.a.cruz20@gmail.com>
2023-04-10 20:41:15 -0500

    Set up files for project

---
+++ tictactoe.js
@@ -0,0 +1,8 @@
+// REVIEW 
+// Using any of the tools you’ve worked with so far, create a game of Tic-Tac-Toe.
+//     1. Create a Tic-Tac-Toe game grid using your HTML element of choice. 
+//     2. When a cell in the grid is clicked, an X or O should appear in that spot depending on whose turn it is.
+//     3. A heading should say whether it is X’s or O’s turn and change with each move made.
+//     4. A button should be available to clear the grid and restart the game.
+//     5. When a player has won, or the board is full and the game results in a draw, a Bootstrap alert or similar Bootstrap component 
+//     should appear across the screen announcing the winner.
